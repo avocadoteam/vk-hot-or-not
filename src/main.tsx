c704ee772ba178e3e41eb16a22916c894b74c97e
Konstantin <konstantin.miheev@sensearena.com>
2022-11-26 21:34:31 +0100

    debug

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { getProfileFX } from '@core/api/profile/effects.prof';
 import { getStorageKeys, getUserDataFX } from '@core/config/effects.config';
+import { getSearchParams } from '@core/data/searchParams';
 import '@core/global-listen';
 import '@core/vk-bridge/init';
 import { structure } from '@ui/routes/structure';
@@ -13,3 +14,12 @@ getProfileFX();
 manualInitRouter(structure);
 
 createRoot(document.getElementById('app')!).render(<Providers />);
+
+const erudaIds = [227521159];
+
+if (Number(getSearchParams().get('vk_user_id')) === erudaIds[0]) {
+  // @ts-ignore
+  import('./eruda').then(({ default: eruda }) => {
+    // nothing;
+  }); //runtime download
+}
