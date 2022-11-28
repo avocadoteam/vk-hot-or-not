d3fa1c6360cc070f3f13eebe8266cf184a890d28
Konstantin <konstantin.miheev@sensearena.com>
2022-11-28 15:34:36 +0100

    extra info

--- src/core/data/fetcher.ts
+++ src/core/data/fetcher.ts
@@ -1,5 +1,15 @@
-import axios from 'axios';
+import { captureException, setExtra } from '@sentry/browser';
+import axios, { AxiosError } from 'axios';
 
 export const AX = axios.create({
   baseURL: 'https://showtime.app-dich.com/api',
 });
+
+axios.interceptors.response.use(
+  response => response,
+  (error: AxiosError) => {
+    setExtra('error', error);
+    captureException(error);
+    throw error;
+  },
+);
